763d14ae722beb5300ac3b08b0b28374da0d1723
mederian <ole.snekkermo@gmail.com>
2020-05-18 10:28:41 +0200

    Added prefabs from old project

---
+++ TowerDefense2020/Assets/UI/Scripts/SelectionResponse/CircleSelectionResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSelectionResponse : MonoBehaviour, ISelectionResponse
+{
+    public void OnDeselect(Transform selection)
+    {
+        var circle = selection.GetComponent<TowerHighlighter>();
+        if(circle != null)
+        {
+            circle.HideCircle();
+        }
+    }
+
+    public void OnSelect(Transform selection)
+    {
+        var circle = selection.GetComponent<TowerHighlighter>();
+        if (circle != null)
+        {
+            circle.ShowCircle();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

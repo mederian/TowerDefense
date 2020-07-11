8f46639652b0e5ab86b3b2d5d68db8bfd8c36552
mederian <ole.snekkermo@gmail.com>
2020-07-11 09:57:48 +0200

    Added ResourceController
    ResourceController hold scriptableobjects for resources and essences.

---
+++ TowerDefense2020/Assets/UI/Scripts/ResourceController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    [SerializeField] ResourceScriptableObject resource;
+    [SerializeField] ResourceScriptableObject essence;
+    [SerializeField] TextMeshProUGUI UIText;
+    [SerializeField] Image EssenceBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIText.text = resource.Value.ToString();
+
+
+        EssenceBar.fillAmount = EssenceToBar();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIText.text = resource.Value.ToString();
+
+
+        EssenceBar.fillAmount = EssenceToBar();
+
+        
+    }
+
+    public float EssenceToBar()
+    {
+        if(essence.Value > 0)
+        {
+            float r = (float)essence.Value;
+            float rs = r / 10.0f;
+
+            return rs;
+        }
+
+        return 0;
+    }
+}
